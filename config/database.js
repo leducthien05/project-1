3dec6ad7444866e7151ee8ae49fd02ac87ec382a
le Duc Thien <ducthien09092005@gmail.com>
2026-03-17 00:48:08 +0700

    fix vercel

--- config/database.js
+++ config/database.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 module.exports.connect = async ()=>{
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, {
+            serverSelectionTimeoutMS: 30000
+        });
         console.log("Connect success");
     } catch (error) {
         console.log(error);
